3ee4665f9187246470e25817785fbfbe493a5f7e
COllins <collinsmuiruri77@gmail.com>
2023-02-08 17:40:36 +0300

    Customized the lucky number and the responses to guesses

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+
+document.querySelector(".number").textContent = secretNumber;
+
+let score = 20
+
 document.querySelector(".check").addEventListener(
     'click', function(){
       const guess = Number(document.querySelector(".guess").value)
       
       if (!guess){
       document.querySelector(".message").textContent = "Sorry, not a number"
+      } else if (guess === secretNumber){
+        document.querySelector(".message").textContent = "Congradulations!";
+      } else if (guess > secretNumber){
+        document.querySelector(".message").textContent = "Higher than secret number";
+        score -=1;
+        document.querySelector(".score").textContent = score;
+      }else if (guess < secretNumber){
+        document.querySelector(".message").textContent = "Lower than secret number";
+        score --;
+        document.querySelector(".score").textContent = score;
       }
     }
 )
